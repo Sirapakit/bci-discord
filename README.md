286aa7b260113289ab823a96c9041be56d54cd1a
Sirapakit <sirapakit.lim@student.mahidol.edu>
2022-07-10 04:07:58 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# bci-discord
-for BCI Lab
+# bci-discord-bot
+for BCI Lab <br >
+
+The purpose of this bot is to 
+<br>1. welcome members 
+<br>2. list member in voice channel 
+<br>3. get the incoming event then notify all members
+
+
+## Documentation
+
+[Discord.js](https://discord.js.org/#/docs/discord.js/main/general/welcome)
+
+
+## Installation
+
+Install the project with node and npm 
+
+```bash
+ npm install discord.js @discordjs/rest
+ npm i dotenv
+```
---
+++ src/bot.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+require("dotenv").config({ path: path.resolve(__dirname, '../.env') });
+console.log(process.env.DISCORD_TOKEN);
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client(
+    { intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+    
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+  })
+  
+  client.on('message', (msg) => {
+    if (msg.content === 'ping') {
+      msg.reply('pong')
+    }
+  })
+
+
+
+client.login(process.env.DISCORD_TOKEN);
+
+
+
+
